33f242ad5f3fa2ddc3739a9cff0944320d180dd0
Michael Pieschel <sixtyhectares@yahoo.com>
2018-07-22 03:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function destructivelyRemoveLastKitten(Kittens) {
 function destructivelyRemoveFirstKitten(Kittens) {
   return kittens.shift();
 }
-function prependKitten(kittens, Arnold) {
-  var kittens = []
+function prependKitten(Arnold) {
   var newArray = kittens = ["Arnold",...kittens];
   return newArray;
 }
