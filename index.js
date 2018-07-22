326d40c0498aea768ae7d961e806661fa4c90d8e
Michael Pieschel <sixtyhectares@yahoo.com>
2018-07-22 02:56:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions and code here
+function destructivelyAppendKitten(kitten) {
+  return kittens.push(kitten);
+}
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten);
+}
+
+function destructivelyRemoveLastKitten(Kittens) {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten(Kittens) {
+  return kittens.shift();
+}
+function appendKitten(kitten) {
+  var newArray = kittens.push(kitten);
+  return newArray;
+}
+function prependKitten(kitten) {
+  var newArray = kittens.unshift(kitten);
+  return newArray;
+}
+function removeLastKitten(kittens) {
+  var newArray = kittens.slice(0, kittens - 2);
+  return newArray;
+}
