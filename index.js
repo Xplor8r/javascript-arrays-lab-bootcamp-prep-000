40fc8e0ee7501c35f54950e7133202974a7c3a1a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-07-22 03:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function prependKitten(kitten) {
   return newArray;
 }
 function removeLastKitten(Kittens) {
-  var newArray = kittens.slice(0, Kittens.length - 1);
+  var newArray = kittens.slice(0, kittens.length - 1);
   return newArray;
 }
